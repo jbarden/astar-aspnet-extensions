cb7472e20312ac25d34ece20efc8bcf8f34008d8
jbarden <jason.barden@outlook.com>
2024-04-12 16:25:15 +0100

    Refactor the GlobalExceptionHandler to use the .Net 8 IExceptionHandler interface

---
+++ src/AStar.ASPNet.Extensions/Handlers/GlobalExceptionHandlerExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace AStar.ASPNet.Extensions.Handlers;
+
+/// <summary>
+/// The <see href="GlobalExceptionHandlerExtensions"></see> class centralises the Global Exception handler consumption.
+/// </summary>
+public static class GlobalExceptionHandlerExtensions
+{
+    /// <summary>
+    /// This method extends the <see href="IServiceCollection"></see> to allow for a fluent, simpler addition of the custom <see href="GlobalExceptionHandler"></see> middleware.
+    /// </summary>
+    /// <param name="services">
+    /// </param>
+    /// <returns>
+    /// The original <see href="IApplicationBuilder"></see> instance for further method chaining.
+    /// </returns>
+    public static IServiceCollection AddGlobalExceptionHandler(this IServiceCollection services)
+
+                                            => services.AddExceptionHandler<GlobalExceptionHandler>();
+}
